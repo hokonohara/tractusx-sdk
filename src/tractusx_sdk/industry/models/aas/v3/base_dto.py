c7fbc1460a36e75004390a06657cb2bb6d8af047
Borja Gómez <borjagomez2003@gmail.com>
2025-04-03 17:27:28 +0200

    feat(DTR CRUD): Adds the pending methods of the AAS API spec (#74)
    * feat: Add methods for updating and deleting Asset Administration Shell and Submodel Descriptors
    
    * feat: Add methods for GET, PUT and DELETE asset ID links for Asset Administration Shell
    
    * feat: Add get description method and its corresponding models
    
    * chore: Add ValidationError to AasService methods docstrings for pydantic validation errors
    
    * fix: Update update_asset_administration_shell_descriptor method to accept ShellDescriptor as parameter
    
    * fix: Update response parsing in AasService methods to return SpecificAssetId list

--- src/tractusx_sdk/industry/models/aas/v3/base_dto.py
+++ src/tractusx_sdk/industry/models/aas/v3/base_dto.py
@@ -22,6 +22,7 @@
 
 from enum import Enum
 from tractusx_sdk.industry.models.aas import (
+    AbstractServiceDescription,
     AbstractPaginatedResponse,
     AbstractPagingMetadata,
     AbstractGetAllShellDescriptorsResponse,
@@ -46,6 +47,49 @@ class MessageTypeEnum(str, Enum):
     EXCEPTION = "Exception"
 
 
+class ProfileEnum(str, Enum):
+    """
+    The Description object enables servers to present their capabilities to the clients, in particular which profiles they implement. At least one defined profile is required. AAS 3.0 specification.
+    """
+
+    ASSETADMINISTRATIONSHELLSERVICESPECIFICATION_SSP_001 = "https://admin-shell.io/aas/API/3/0/AssetAdministrationShellServiceSpecification/SSP-001"
+    ASSETADMINISTRATIONSHELLSERVICESPECIFICATION_SSP_002 = "https://admin-shell.io/aas/API/3/0/AssetAdministrationShellServiceSpecification/SSP-002"
+    SUBMODELSERVICESPECIFICATION_SSP_001 = (
+        "https://admin-shell.io/aas/API/3/0/SubmodelServiceSpecification/SSP-001"
+    )
+    SUBMODELSERVICESPECIFICATION_SSP_002 = (
+        "https://admin-shell.io/aas/API/3/0/SubmodelServiceSpecification/SSP-002"
+    )
+    SUBMODELSERVICESPECIFICATION_SSP_003 = (
+        "https://admin-shell.io/aas/API/3/0/SubmodelServiceSpecification/SSP-003"
+    )
+    AASXFILESERVICESPECIFICATION_SSP_001 = (
+        "https://admin-shell.io/aas/API/3/0/AasxFileServerServiceSpecification/SSP-001"
+    )
+    ASSETADMINISTRATIONSHELLREGISTRYSERVICESPECIFICATION_SSP_001 = "https://admin-shell.io/aas/API/3/0/AssetAdministrationShellRegistryServiceSpecification/SSP-001"
+    ASSETADMINISTRATIONSHELLREGISTRYSERVICESPECIFICATION_SSP_002 = "https://admin-shell.io/aas/API/3/0/AssetAdministrationShellRegistryServiceSpecification/SSP-002"
+    SUBMODELREGISTRYSERVICESPECIFICATION_SSP_001 = "https://admin-shell.io/aas/API/3/0/SubmodelRegistryServiceSpecification/SSP-001"
+    SUBMODELREGISTRYSERVICESPECIFICATION_SSP_002 = "https://admin-shell.io/aas/API/3/0/SubmodelRegistryServiceSpecification/SSP-002"
+    DISCOVERYSERVICESPECIFICATION_SSP_001 = (
+        "https://admin-shell.io/aas/API/3/0/DiscoveryServiceSpecification/SSP-001"
+    )
+    ASSETADMINISTRATIONSHELLREPOSITORYSERVICESPECIFICATION_SSP_001 = "https://admin-shell.io/aas/API/3/0/AssetAdministrationShellRepositoryServiceSpecification/SSP-001"
+    ASSETADMINISTRATIONSHELLREPOSITORYSERVICESPECIFICATION_SSP_002 = "https://admin-shell.io/aas/API/3/0/AssetAdministrationShellRepositoryServiceSpecification/SSP-002"
+    SUBMODELREPOSITORYSERVICESPECIFICATION_SSP_001 = "https://admin-shell.io/aas/API/3/0/SubmodelRepositoryServiceSpecification/SSP-001"
+    SUBMODELREPOSITORYSERVICESPECIFICATION_SSP_002 = "https://admin-shell.io/aas/API/3/0/SubmodelRepositoryServiceSpecification/SSP-002"
+    SUBMODELREPOSITORYSERVICESPECIFICATION_SSP_003 = "https://admin-shell.io/aas/API/3/0/SubmodelRepositoryServiceSpecification/SSP-003"
+    SUBMODELREPOSITORYSERVICESPECIFICATION_SSP_004 = "https://admin-shell.io/aas/API/3/0/SubmodelRepositoryServiceSpecification/SSP-004"
+    CONCEPTDESCRIPTIONSERVICESPECIFICATION_SSP_001 = "https://admin-shell.io/aas/API/3/0/ConceptDescriptionServiceSpecification/SSP-001"
+
+
+class ServiceDescription(AbstractServiceDescription, VersionedModel):
+    """
+    The Description object enables servers to present their capabilities to the clients, in particular which profiles they implement. Following the AAS 3.0 specification.
+    """
+
+    pass
+
+
 class PagingMetadata(AbstractPagingMetadata, VersionedModel):
     """Paging metadata for the response following the AAS 3.0 specification."""
 
@@ -61,7 +105,7 @@ class PaginatedResponse(AbstractPaginatedResponse[PagingMetadata], VersionedMode
 class GetAllShellDescriptorsResponse(
     PaginatedResponse,
     AbstractGetAllShellDescriptorsResponse[PagingMetadata, ShellDescriptor],
-    VersionedModel
+    VersionedModel,
 ):
     """Response model for the get_all_shell_descriptors method following the AAS 3.0 specification."""
 
@@ -71,7 +115,7 @@ class GetAllShellDescriptorsResponse(
 class GetSubmodelDescriptorsByAssResponse(
     PaginatedResponse,
     AbstractGetSubmodelDescriptorsByAssResponse[PagingMetadata, SubModelDescriptor],
-    VersionedModel
+    VersionedModel,
 ):
     """Response model for the get_submodel_descriptors method following the AAS 3.0 specification."""
 
